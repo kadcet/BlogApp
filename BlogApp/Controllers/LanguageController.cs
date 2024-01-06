ffe8efafd0779fe5b6f0f868e82cddbf65cad2a4
Kadir Çetin <kadirrcetin@gmail.com>
2024-01-07 02:53:01 +0300

    070124-1

---
+++ BlogApp/Controllers/LanguageController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApp.Controllers
+{
+    public class LanguageController : Controller
+    {
+        public IActionResult SetLanguage(string culture,string returnURL)
+        {
+            Response.Cookies.Append(
+
+                CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+                new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(10) });
+            return LocalRedirect(returnURL);
+        }
+    }
+}
--- BlogApp/Program.cs
+++ BlogApp/Program.cs
@@ -1,14 +1,44 @@
 using BlogApp.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Globalization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews()
+    .AddMvcLocalization(Microsoft.AspNetCore.Mvc.Razor.LanguageViewLocationExpanderFormat.Suffix);
+
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+var AllCultures = new List<CultureInfo>
+{
+   new CultureInfo("tr-TR"),
+   new CultureInfo("en-US"),
+   new CultureInfo("fr-FR")
+};
+
+var defaultCulture = new Microsoft.AspNetCore.Localization.RequestCulture("tr-TR");
+
+
+builder.Services.Configure<RequestLocalizationOptions>(options =>
+{
+    options.SupportedCultures = AllCultures;
+    options.SupportedUICultures = AllCultures;
+    options.DefaultRequestCulture = defaultCulture;
+});
+
+
 builder.Services.AddDbContext<BlogDbContext>(opt=>
 opt.UseSqlServer("Server=.;Database=BlogDb;Integrated Security=True;TrustServerCertificate=true"));
 
 var app = builder.Build();
+
+app.UseRequestLocalization(options =>
+{
+    options.SupportedCultures = AllCultures;
+    options.SupportedUICultures = AllCultures;
+    options.DefaultRequestCulture = defaultCulture;
+});
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
